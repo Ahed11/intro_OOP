5b06410fa1a4e1cb20cb3df82f3ee23394ceae65
Ahd <ahdghandi73@gmail.com>
2025-05-06 09:28:03 +0300

    Обновление фалйа

--- intro_OOP.php
+++ intro_OOP.php
@@ -1,99 +1,218 @@
 <?php
 #Для запуска ввести в трминале "С:\xampp\htdocs\intro_OOP.php"
+declare(strict_types=1);
 
-class Book {
-    
-    public $name;
-    public $author;
-    public $dateOfPublication;
+abstract class PrintedMaterial {
+    protected string $title;
+    protected string $author;
+    protected int $year;
 
-    public function __construct($name, $author, $dateOfPublication){
-        $this->setName($name);
-        $this->setAuthor($author);
-        $this->setDateOfPublication($dateOfPublication);
-    }
-
-    public function setName($name){
-        $this->name = $name;
-    }
-
-    public function setAuthor($author){
+    public function __construct(string $title, string $author, int $year) {
+        $this->title = $title;
         $this->author = $author;
+        $this->year = $year;
     }
 
-    public function setDateOfPublication($dateOfPublication){
-        $this->dateOfPublication = $dateOfPublication;
+    public function getTitle(): string {
+        return $this->title;
     }
 
-    public function isAvailable(){
-
+    public function getInfo(): string {
+        return "{$this->title} ({$this->year}) by {$this->author}";
     }
+}
 
-    public function assignToUser(){
+interface Loanable {
+    public function borrow(User $user): bool;
+    public function returnBook(): void;
+    public function isAvailable(): bool;
+}
 
-    }
+class Book extends PrintedMaterial implements Loanable{
     
-    public function returnBook(){
+    private ?User $borrowedBy = null;
 
+    public function borrow(User $user): bool {
+        if ($this->borrowedBy !== null) {
+            return false;
+        }
+        $this->borrowedBy = $user;
+        return true;
+    }
+
+    public function returnBook(): void {
+        $this->borrowedBy = null;
+    }
+
+    public function isAvailable(): bool {
+        return $this->borrowedBy === null;
+    }
+
+    public function getBorrower(): ?User {
+        return $this->borrowedBy;
     }
 }
 
 class User {
 
-    public $name;
-    public $borrowedBooks = [];
+    private string $name;
+    private array $borrowedBooks = [];
 
-        
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function borrowBook(Book $book): bool {
+        if ($book->borrow($this)) {
+            $this->borrowedBooks[] = $book;
+            return true;
+        }
+        return false;
+    }
+
+    public function returnBook(Book $book): void {
+        $book->returnBook();
+        // Удалить книгу из списка взятых
+        foreach ($this->borrowedBooks as $index => $b) {
+            if ($b === $book) {
+                unset($this->borrowedBooks[$index]);
+                $this->borrowedBooks = array_values($this->borrowedBooks);
+            }
+        }
+    }
+
+    public function getBorrowedBooks(): array {
+        return $this->borrowedBooks;
+    }
 }
 
 class Library {
-    
-    private $books = [];
-    private $users = [];
-    
-    public function addBook(Book $book){
+    private array $books = [];
+    private array $users = [];
+
+    public function addBook(Book $book): void {
         $this->books[] = $book;
     }
 
-    public function getBooks(){
+    public function registerUser(User $user): void {
+        $this->users[] = $user;
+    }
+
+    public function getBooks(): array {
         return $this->books;
+    }
+
+    public function getUsers(): array {
+        return $this->users;
     }
 }
 
-class BookInputHandler {
-    
-    public static function inputBooks(){
-    
-        $books = [];
-    
-        while(true){
-            $name = readline("Введите название книги (или \"стоп\" для завершения): ");
-            if($name == "стоп"){
-                break;
-            }
-            $author = readline("Автор: ");
-            $dateOfPublication = readline("Год издания: ");
-            $books[] = [
-                'name' => $name,
-                'author' => $author,
-                'dateOfPublication' => $dateOfPublication
-            ];
-        }
-    
-        return $books;
-    }
-}
+#
+#
+# НИже реализуются код без классов
+#
+#
 
 echo "Добро пожаловать в нашу библиотеку\n\n";
 echo "---Давайте добавим ваши книги---\n\n";
 
 $library = new Library();
-$inputBooks = BookInputHandler::inputBooks();
 
-foreach($inputBooks as $bookData){
-    $book = new Book($bookData['name'], $bookData['author'], $bookData['dateOfPublication']);
-    $library->addBook($book);
+while(true){
+    $titleOfBook = readline("Введите название вашей книги (или \"стоп\" для завершения): ");
+    
+    if($titleOfBook === "стоп"){
+        break;
+    }
+
+    $authorOfBook = readline("Введите автора");
+    $year = (int)readline("Введите год издания");
+
+    $nBook = new Book($titleOfBook, $authorOfBook, $year);
+    $library->addBook($nBook);
+}
+
+echo "Давайте теперь зарегистрируем пользователей";
+
+while(true){
+    $nameOfUser = readline("Введите имя пользователя (или \"стоп\" для завершения): ");
+
+    if($nameOfUser === "стоп"){
+        break;
+    }
+
+    $nUser = new User($nameOfUser);
+    $library->registerUser($nUser);
+}
+
+echo "\n\n--- Действия ---\n\n";
+
+echo "выберите из следующих пользователей какой вам нужен, чтобы подобрать для него нужные книги\n\n";
+
+$users = $library->getUsers();
+$books = $library->getBooks();
+
+while(true){
+
+    $stopper = readline("Хотите изменить владения пользователей книгами? (y/n): ");
+    if($stopper === "y"){
+        foreach($users as $user){
+
+            $choiceOfUser = readline("Выбираете ли этого пользователя: " . $user->getName() . " ? (y/n): ");
+            if($choiceOfUser === "y"){
+                
+                $choiceOfAction = (string)readline("Вы хотите взять(1) или вернуть(2) книгу: ");
+                if($choiceOfAction === "1"){
+                    foreach($books as $book){
+        
+                        $choiceOFBook = readline("Выбираете ли эту книгу: " . $book->getInfo() . " ? (y/n): ");
+                        if($choiceOFBook === "y"){
+                            if(($user->borrowBook($book)) == true){
+                                echo "Пользователю " . $user->getName() . " была выдана книга: " . $book->getInfo() . "\n";
+                            }else{
+                                echo $user->getName() . " не может взять книгу " . $book->getInfo() . ". Книга уже выдана кому-то другому.\n";
+                            }
+                        }
+                    }
+                }elseif($choiceOfAction === "2"){
+                    foreach($books as $book){
+
+                        $choiceOFBook = readline("Выбираете ли эту книгу: " . $book->getInfo() . " ? (y/n): ");
+                        if($choiceOFBook === "y"){
+                            $user->returnBook($book);
+                            echo "Пользователем " . $user->getName() . " была возвращена книга: " . $book->getInfo() . "\n";
+                        }
+                    }
+                }
+            }
+        }
+    } else{
+        break;
+    }
+}
+
+echo "\n\n--- Статус библиотеки ---\n\n";
+
+echo "Книги в библиотеке:\n\n";
+
+foreach($books as $book){
+    if($book->isAvailable()){
+        echo "Книга \"" . $book->getTitle() . "\" - свободна\n";
+    }else{
+        echo "Книга \"" . $book->getTitle() . "\" на руках у " . $book->getBorrower()->getName() . "\n";
+    }
 }
 
 
+echo "Книги на руках у пользователей:\n\n";
+
+foreach($users as $user){
+    foreach($user->getBorrowedBooks() as $book){
+        echo $user->getName() . ": " . $book->getTitle() . "\n";
+    }
+}
 ?>
